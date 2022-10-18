0867b35cff5d14f9b7eae9bf517ab4c1f2040bb8
ViharevN <swix_off@mail.ru>
2022-10-18 23:46:14 +0300

    массивы часть 2. домашнее задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
-
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        //задание 1
+
+        int [] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+
+        }
+        System.out.println("Сумма трат за месяц составила "+ sum +" рублей");
+
+    }
+    public static int[] generateRandomArray() {
+        Random random = new Random();
+        int[] arr =new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
 
     }
 }
