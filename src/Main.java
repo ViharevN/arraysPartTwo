b85652d2721d981f04103eea32d22cbf5b248eec
ViharevN <swix_off@mail.ru>
2022-10-19 00:17:54 +0300

    массивы часть 2. домашнее задание 2

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,24 @@ public class Main {
         }
         System.out.println("Сумма трат за месяц составила "+ sum +" рублей");
 
+        //задание 2
+
+        int minSpending = 200_000;//указываем большее значение, т.к. ищем минимальное значение в массиве
+        int maxSpending = 0;//указываем меньшее значение, т.к. ищем большее значение. В эту переменнуюю запишем наибольшее значение
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+            if (arr[i] < minSpending) {
+                minSpending = arr[i];
+            }
+            if (arr[i]>maxSpending) {
+                maxSpending=arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила "+ minSpending +" рублей. Максимальная сумма трат за день составила "+ maxSpending +" рублей");
+
+
+
+
     }
     public static int[] generateRandomArray() {
         Random random = new Random();
